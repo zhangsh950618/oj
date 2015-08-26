53ccdda99588cb66f9cf5aea99104a44dcb2031d
Your Name <zhangsh950618@icloud.com>
2015-08-26 19:11:14 +0800

    refomate

--- PyTip/IP判断.py
+++ PyTip/IP判断.py
@@ -1,24 +1,19 @@
 __author__ = 'zsh'
 
+s = "202.115.32.24"
+S = s.split('.')
 
-
-s="202.115.32.24"
-S=s.split('.')
 
 def judge():
     if len(S) != 4:
         return False
     for ss in S:
         try:
-            if int(ss) not in range(0,256):
-                return  False
+            if int(ss) not in range(0, 256):
+                return False
         except:
             return False
     return True
 
 
 print 'Yes' if judge() else 'No'
-
-
-
-
