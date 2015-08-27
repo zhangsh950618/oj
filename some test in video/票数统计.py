895ec0a3935f27ea604b94c5f8c59d302a79ac83
Your Name <zhangsh950618@icloud.com>
2015-08-27 10:54:16 +0800

    using Counter

--- some test in video/票数统计.py
+++ some test in video/票数统计.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+from collections import Counter
+
 __author__ = 'zsh'
 
 
@@ -6,11 +8,12 @@ __author__ = 'zsh'
 
 
 aStr = "handkerchief'sKJLKZCBl:JKL"
-aDict = {}
+# aDict = {}
 
-for i in aStr:
-    # """ D.setdefault(k[,d]) -> D.get(k,d), also set D[k]=d if k not in D """
-    # 如果不存在就初始化为0
-    aDict.setdefault(i, 0)
-    aDict[i] += 1
-print(aDict)
+# for i in aStr:
+# """ D.setdefault(k[,d]) -> D.get(k,d), also set D[k]=d if k not in D """
+# 如果不存在就初始化为0
+# aDict[i] += 1
+# print(aDict)
+
+print(Counter(aStr))
