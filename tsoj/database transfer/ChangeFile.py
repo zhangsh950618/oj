340572961245b5190b22b0ce9ff09699636a7f9f
张少华 <zhangsh950618@gmai.com>
2015-10-11 09:57:50 +0800

    over

--- tsoj/database transfer/ChangeFile.py
+++ tsoj/database transfer/ChangeFile.py
@@ -13,7 +13,6 @@ for parent, dirnames, filenames in os.walk(rootdir):  # 三个参数：分别返
             print('delete file:', os.path.join(parent, filename))
             os.remove(os.path.join(parent, filename))
             # print "the full name of the file is:" + os.path.join(parent, filename)  # 输出文件路径信息
-
 for parent, dirnames, filenames in os.walk(rootdir):  # 三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
     for filename in filenames:  # 输出文件信息
         if filename == 'in.1':
--- tsoj/import lanqiao/FindWord.py
+++ tsoj/import lanqiao/FindWord.py
@@ -1,16 +1,25 @@
 # coding=utf-8
 
 import os
-
+from docx import Document
 __author__ = 'zsh'
 
 inpath = 'C:\Users\zsh\Documents\Private Files\OnlineJudge\蓝桥杯题库\蓝桥杯题库'
 # print(inpath)
 
 
+
+
 # 中文重新编码
 uipath = inpath.decode('utf-8')
-for list_name in os.walk(uipath):
-    print(list_name)
+for partent, dirnames, filenames in os.walk(uipath):
+    for filename in filenames:
+        # print(filename)6
+        if filename.endswith('.doc') or filename.endswith('.docx'):
+            print(filename)
+            fullname = os.path.join(partent, filename)
+            print(fullname)
+            document = Document(fullname)
+
 
 
